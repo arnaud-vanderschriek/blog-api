9e3f73aa60ae423a298381bdd3ace5808b078bc3
arnaud <arnaud.vanderschrieck@gmail.com>
2025-08-23 13:11:10 +0200

    Added user: controller, route, model and server.js

---
+++ controllers/userController.js
@@ -0,0 +1,51 @@
+const User = require("../models/User");
+
+exports.createUser = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ error: "Post non trouvé" });
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.updateUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!user) return res.status(404).json({ error: "Post non trouvé" });
+    res.json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) return res.status(404).json({ error: "Post non trouvé" });
+    res.json({ message: "Post supprimé ✅" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const postRoutes = require("./routes/posts");
-
+const userRoutes = require("./routes/users");
 dotenv.config();
 connectDB();
 
@@ -13,6 +13,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/api/posts", postRoutes);
+app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT} 🚀`));
